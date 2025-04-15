fee2772f206f4f62f800bbc050faa0eaed6f7d39
Soumya <soumyadippayra@gmail.com>
2025-04-15 23:24:04 +0530

    100 s expiry

--- src/jwts/jwts.go
+++ src/jwts/jwts.go
@@ -15,7 +15,7 @@ func Initialize() error {
 func GenerateToken(username string) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"username": username,
-		"exp":      time.Now().Add(time.Hour * 24).Unix(),
+		"exp":      time.Now().Add(time.Second * 100).Unix(),
 	})
 	secretBytes := secrets.GetSecretKey()
 	tokenString, err := token.SignedString(secretBytes)
